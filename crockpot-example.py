235476e3e387ef6a12d603209285b539d04a31e8
Kevin Carey <careykevin@gmail.com>
2017-07-31 14:46:49 -0400

    Implement random conditions

--- crockpot-example.py
+++ crockpot-example.py
@@ -1,1 +1,12 @@
-print("hello crockpot")
+import time
+from random import randint
+
+RAND = randint(0, 9)
+
+if RAND < 2:
+    raise Exception("Simulated exception")
+elif RAND < 4:
+    print "Simulated delay"
+    time.sleep(30)
+
+print RAND
