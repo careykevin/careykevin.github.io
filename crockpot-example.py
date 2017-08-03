ffa6815da106f424e9a2941ff1092b917ef607bb
Kevin Carey <careykevin@gmail.com>
2017-08-03 16:40:29 -0400

    Print time

--- crockpot-example.py
+++ crockpot-example.py
@@ -1,12 +1,3 @@
-import time
-from random import randint
+import datetime
+print datetime.datetime.now().isoformat()
 
-RAND = randint(0, 9)
-
-if RAND < 2:
-    raise Exception("Simulated exception")
-elif RAND < 4:
-    print "Simulated delay"
-    time.sleep(30)
-
-print RAND
